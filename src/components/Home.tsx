e5e4fdac35da1861e1807d08006336bf0695dfd2
SaiSrinivas321 <gssrinivas32194@gmail.com>
2021-10-11 13:02:44 +0530

    added backgroundimage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,19 +1,20 @@
 import { Box, Center, Container, Flex, Grid, VStack } from '@chakra-ui/layout';
-import { Heading ,Button } from '@chakra-ui/react';
+import { Heading ,Button ,Text} from '@chakra-ui/react';
 import React from 'react';
+import "../styles/Home.css";
 
 const Home = () =>{
     return (
-    <Center minHeight={"100vh"}>
-    <Flex flexDirection={"column"} alignItems="center" justifyItems="center"  paddingTop={['10px','20px']} >
-        <Box bg = "orange" width={'50%'} textColor="white" p={2} m={2}>
+    <Center minHeight={"100vh"} className="home">
+    <Flex flexDirection={"column"} alignItems="center" justifyItems="center">
+        <Box bg = "orange" width={'50%'} borderRadius="20px" textColor="white" p={2} m={2}>
         <Center>
-        <Heading size = "md" >Shaastra - 2022</Heading>
+        <Text className="shaastra-title">SHASSTRA'22</Text>
         </Center>
         </Box>
-        <Heading p={2} m={2}>Tech and Innovation Fair</Heading>
+        <Heading p={2} m={2} size="lg" fontFamily={'Montserrat'} color={"white"}>Tech and Innovation Fair</Heading>
         <Box p={2} m={2}>
-            <Button bg="orange" borderRadius={'20px'} textColor="white">Register Now</Button>
+            <Button bg="orange" borderRadius={'20px'} textColor="white" className="registerbutton">Register Now</Button>
         </Box>
     </Flex>
     </Center>
